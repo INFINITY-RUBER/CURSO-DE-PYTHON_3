6dbf904233a58bff6bed63388500e3644f56f708
RUBER <ruberhenandez@gmail.com>
2019-01-14 22:29:08 -0500

    CLASE 12 .. ESTRUCTURAS CONDICIONALES

--- SERVIDOR/mainprueba.py
+++ SERVIDOR/mainprueba.py
@@ -1,10 +1,14 @@
-clients = 'Pablo, Ricardo'#creamos el string
+clients = 'Pablo, Ricardo' #creamos el string
+
 
 def create_client(client_name):
 	global clients  #Utilizamos global para definir que la variable es la globarl, es decir la que definimos con pablo y ricardo
-    clients += client_name  #adicionamos el nuevo string
-    _add_coma() #ejecutamos la funcion _add_coma para agregar una coma y un espacio
 
+	if client_name not in clients:
+		clients += client_name
+		_add_coma()
+	else:
+		print('client already is the client\'s list')
 
 def _add_coma():#el nombre de la función comienza con un guión bajo para establecer que será una funcion privada
 	global clients
@@ -14,26 +18,22 @@ def list_clients():#función que muestra la lista de clientes
 	global clients
 	print (clients) #imprimimos el string clientes
 
+def _print_welcome():
+	 print('WELCOME TO PLATZI VENTAS')
+	 print('*' * 50)
+	 print('what would you to do today')
+	 print('[C]reate cliente')
+	 print('[D]elete cliente')
+
 
 if __name__ == '__main__': #funcion main
-	print("Primer lista")
-	list_clients() #Listamos los clientes
-	print("****************************************************")
-	create_client('ruber') #ejecutamos la funcion crear cliente
-	print("Lista despues de ejecutar la función create_client()")
-	list_clients() #Volvemos a ejecutar la funcion listar clientes
-
-	print("****************************************************")
-	print ('Funcion lambda')
-	#Funcion lambda
-	"""
-	Se trata de crear funciónes de manera rápida, just in time, sobre la marcha, para prototipos
-	ligeros que requieren únicamente de una pequeña operación o comprobación. Por lo tanto,
-	toda funcioón lambda también puede expresarse como una convencional (pero no viceversa)
-	f = lambda argumentos: resultado
-	"""
-	f = lambda x,y: x+y
-	print (f)
-
-
-input() #En este caso la usamos para pausar la ejecución del programa hasta presionar la tecla enter
+	_print_welcome()
+	command = input()
+	if command == 'C':
+		client_name = input('what is the cliente name? ')
+		create_client(client_name)
+		list_clients()	     #Listamos los clientes
+	elif command == 'D':
+		pass
+	else:
+		print('invalid command')
